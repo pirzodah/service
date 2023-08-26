c02acdcf1ae7a3f52b9eda65a7d6e719b7f6c9e7
Eydzhpee08 <pirovhakim37@gmail.com>
2023-08-26 19:20:29 +0500

    fixed some

--- internal/models/config.go
+++ internal/models/config.go
@@ -3,7 +3,7 @@ package models
 type Configuration struct {
 	Version     string `json:"version"`
 	Api         string `json:"api"`
-	DatabaseUrl string `json:"DATABASE_URL"`
+	PostgresDsn string `json:"postgresDsn"`
 	MssqlDsn    string `json:"mssqlDsn"`
 	Host        string `json:"host"`
 	Port        string `json:"port"`
